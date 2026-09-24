719525ac855c1d6e5ce8cfabe523833a0df97086
Yaron Serlin <86887011+yaronserlin@users.noreply.github.com>
2026-09-24 21:03:58 +1000

    Mount accessibility menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { AutomataEditorApp } from './AutomataEditorApp.js';
+import { AccessibilityMenu } from './ui/AccessibilityMenu.js';
 
 new AutomataEditorApp(document);
+new AccessibilityMenu(document).mount();
